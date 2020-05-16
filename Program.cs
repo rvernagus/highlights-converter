5fe5fbccb659c842b1d7788d867ec9947d6bf8e2
Ray Vernagus <rvernagus@hotmail.com>
2020-05-16 01:53:15 +0000

    Create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace highlights_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
